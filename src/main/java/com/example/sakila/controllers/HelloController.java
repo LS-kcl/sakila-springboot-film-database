602a0d51efb2f08031c7de0b0fbed4f86e348297
Luke <lbmoogs@gmail.com>
2024-04-15 15:47:53 +0100

    add: Add initial files to project

---
+++ src/main/java/com/example/sakila/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.sakila.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    /*
+    @GetMapping("/greeting/{id}")
+    public String greet(@PathVariable(name="id") Integer id) {
+        return "Hello world! ID is: " + id;
+    }
+     */
+
+    @GetMapping("/greeting")
+    public String greet(@RequestParam Integer page) {
+        return "Hello world! page is: " + page;
+    }
+}
